1ffb14a5d423a1336e1efc3d66d207208ff08fc3
grotius.hasiholan <grotius.hasiholan@gmail.com>
2022-02-07 21:22:16 +0700

    Editing login form

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Button } from 'reactstrap';
-import { Form, FormGroup, Label, Input} from 'reactstrap';
+import { Form, FormGroup, Label, Input, Card, CardBody, CardTitle,Row, Col} from 'reactstrap';
 import Navbar from './partials/Navbar'
 
 
@@ -10,41 +10,57 @@ function LoginPage() {
   return (
     <>
       <Navbar />
-      <Form inline>
-        <FormGroup>
-          <Label
-            for="exampleEmail"
-            hidden
+      <Row className='justify-content-center'>
+        <Col xs='11' sm='8' xl='5' className='text-center'>
+          <Card className='mt-sm-4'
+            
+            style={{
+              borderRadius: '10px'
+            }}
           >
-            Email
-          </Label>
-          <Input
-            id="exampleEmail"
-            name="email"
-            placeholder="Email"
-            type="email"
-          />
-        </FormGroup>
-        {' '}
-        <FormGroup>
-          <Label
-            for="examplePassword"
-            hidden
-          >
-            Password
-          </Label>
-          <Input
-            id="examplePassword"
-            name="password"
-            placeholder="Password"
-            type="password"
-          />
-        </FormGroup>
-        <Button>
-          Login
-        </Button>
-      </Form>
-
+            <CardBody className='p-5'>
+              <CardTitle tag='h2'>
+                Login
+              </CardTitle>
+              <Form className='mt-5' inline>
+                <FormGroup>
+                  <Label
+                    for="exampleEmail"
+                    hidden
+                  >
+                    Email
+                  </Label>
+                  <Input
+                    id="exampleEmail"
+                    name="email"
+                    placeholder="Email"
+                    type="email"
+                  />
+                </FormGroup>
+                {' '}
+                <FormGroup>
+                  <Label
+                    for="examplePassword"
+                    hidden
+                  >
+                    Password
+                  </Label>
+                  <Input
+                    id="examplePassword"
+                    name="password"
+                    placeholder="Password"
+                    type="password"
+                  />
+                </FormGroup>
+                <Button>
+                  Login
+                </Button>
+              </Form>
+            </CardBody>
+          </Card>
+        </Col>
+      </Row>
+      
   </>
   );
 }
